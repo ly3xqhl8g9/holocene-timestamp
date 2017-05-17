18ea4482af287e58a06771feefd3bae3c2e0e7fc
ly3xqhl8g9 <ly3xqhl8g9@plurid.com>
2017-05-17 17:51:01 +0300

    Options.js

---
+++ HEA-Chrome/js/popup_script.js
@@ -0,0 +1,25 @@
+// chrome.storage.sync.get('activeOptions', function(element) {
+//     if (element['activeOptions'] == 'off') {
+//         document.getElementById('extensionOff').setAttribute("checked", "checked");
+//     } else {
+//         document.getElementById('extensionOn').setAttribute("checked", "checked");
+//     }
+//     // console.log(element['activeOptions']);
+// });
+
+// document.getElementById('extensionOn').click(
+//     chrome.storage.sync.set({activeOptions: 'onWiki'});
+// );
+
+// document.getElementById('extensionOff').click(
+//
+// );
+
+
+// chrome.storage.sync.set({activeOptions: 'off'}, function(element) {
+//         document.getElementById('extensionOff').click();
+// });
+//
+// chrome.storage.sync.set({activeOptions: 'onWiki'}, function(element) {
+//         document.getElementById('extensionOn').click();
+// });
