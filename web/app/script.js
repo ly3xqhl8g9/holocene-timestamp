9ad3afeaac4d04a1956bdbd8ce20aa22f30f7230
Jan Cavel <caveljan@gmail.com>
2018-12-12 16:19:43 +0200

    dev: web app

--- web/app/script.js
+++ web/app/script.js
@@ -19,11 +19,9 @@ function changeBackgroundOnScroll() {
 function setTabs() {
     let tabAbout = document.getElementById("tab-about");
     let tabConversion = document.getElementById("tab-conversion");
-    let tabGift = document.getElementById("tab-gift");
 
     let contentAbout = document.getElementById("content-about");
     let contentConversion = document.getElementById("content-conversion");
-    let contentGift = document.getElementById("content-gift");
 
 
     tabAbout.addEventListener("click", function() {
@@ -32,7 +30,7 @@ function setTabs() {
         contentAbout.style.display = "block";
         contentConversion.style.display = "none";
         contentGift.style.display = "none";
-    })
+    });
 
     tabConversion.addEventListener("click", function() {
         setActiveClassToTab(this);
@@ -40,15 +38,7 @@ function setTabs() {
         contentConversion.style.display = "block";
         contentAbout.style.display = "none";
         contentGift.style.display = "none";
-    })
-
-    tabGift.addEventListener("click", function() {
-        setActiveClassToTab(this);
-
-        contentGift.style.display = "block";
-        contentAbout.style.display = "none";
-        contentConversion.style.display = "none";
-    })
+    });
 }
 
 
@@ -63,78 +53,6 @@ function setActiveClassToTab(element) {
     element.classList.add("active");
 }
 
-
-////////
-// GIFT
-function expandGiftDrawer() {
-    let giftCryptoExpand = document.getElementById("gift-crypto-expand");
-    let giftCardCrypto = document.getElementById("gift-card-crypto");
-    let giftCardDrawer = document.getElementById("gift-card-drawer");
-
-    giftCryptoExpand.addEventListener("click", () => {
-        if (giftCardCrypto.style.height == "450px") {
-            giftCryptoExpand.innerHTML = "&#9660;";
-            giftCardCrypto.style.height = "200px";
-            giftCardDrawer.style.display = "none";
-        } else {
-            giftCryptoExpand.innerHTML = "&#9650;";
-            giftCardCrypto.style.height = "450px";
-            giftCardDrawer.style.display = "block";
-            // window.scrollTo(0,document.body.scrollHeight);
-        }
-    });
-}
-
-
-function copyCryptosOnClick() {
-    function copyBtcAdressOnClick() {
-        let btc = document.getElementById("btc-address");
-        let address = "1CKoEnMU3Hy1UiWfJVBTruVSmrLvhDkXd6"
-        copyCryptoAddress(btc, address)
-    }
-
-    function copyEthAdressOnClick() {
-        let eth = document.getElementById("eth-address");
-        let address = "0x91936fc5A7e13ae96273E371fD3407B0F3ABc553"
-        copyCryptoAddress(eth, address)
-    }
-
-    function copyBchAdressOnClick() {
-        let bch = document.getElementById("bch-address");
-        let address = "qrlar2grfap9n86gfrm6z69yw56ezv35zyts7ufxjv"
-        copyCryptoAddress(bch, address)
-    }
-
-    function copyCryptoAddress(crypto, address) {
-        crypto.addEventListener("click", () => {
-            let range = document.createRange();
-            range.selectNode(crypto);
-            let selection = window.getSelection()
-            selection.removeAllRanges();
-            selection.addRange(range);
-
-            try {
-                if (crypto.innerHTML == address) {
-                    document.execCommand('copy');
-                }
-                crypto.innerHTML = `<span class="address-copy">address<br>copied</span>`;
-                setTimeout(function() {
-                    crypto.innerText = address;
-                }, 2000);
-            } catch(err) {
-                crypto.innerHTML = `<span class="address-copy">address<br>couldn't be copied</span>`;
-                setTimeout(function() {
-                    crypto.innerText = address;
-                }, 2000);
-            }
-            window.getSelection().removeAllRanges();
-        });
-    }
-
-    copyBtcAdressOnClick();
-    copyEthAdressOnClick();
-    copyBchAdressOnClick();
-}
 
 
 ///////////////////
@@ -398,6 +316,10 @@ function setHoloceneYears(text) {
 
 
 function setConversionFact(factString, factText, year) {
+    if (year === undefined) {
+        year = 2018;
+    }
+
     function capitalizeFirstLetter(string) {
         return string.charAt(0).toUpperCase() + string.slice(1);
     }
@@ -462,9 +384,11 @@ function closeOpenFacts(title, content) {
 function init() {
     changeBackgroundOnScroll();
     setTabs();
-    expandGiftDrawer();
-    copyCryptosOnClick();
     conversionLogic()
+
+    let inputYear = '2018';
+    let HEyear = '12018';
+    fetchYearFacts(inputYear, HEyear)
 }
 
 init();
