72766c0f7d1050d90f90dbd3565a5f40d3f7e67a
Jan Cavel <caveljan@gmail.com>
2019-08-03 10:00:59 +0300

    chrome > setup: build

---
+++ packages/chrome/src/components/Popup/index.tsx
@@ -0,0 +1,23 @@
+import * as React from 'react';
+
+import {
+    StyledPopup,
+} from './styled';
+
+
+
+const Popup: React.FC<any> = (properties) => {
+
+    // const openOptions = () => {
+    //     chrome.runtime.openOptionsPage();
+    // }
+
+    return (
+        <StyledPopup>
+            holocene timestamp
+        </StyledPopup>
+    );
+}
+
+
+export default Popup;
