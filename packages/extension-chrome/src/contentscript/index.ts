ed5636ac585ba4b3ecbe55f22dbce7b188ac167a
Jan Cavel <caveljan@gmail.com>
2019-08-04 14:48:22 +0300

    extension-chrome > dev: parser

--- packages/extension-chrome/src/contentscript/index.ts
+++ packages/extension-chrome/src/contentscript/index.ts
@@ -1,14 +1,25 @@
-import HoloceneParser from './Parser';
+import HoloceneTimestampParser, {
+    HoloceneTimestampParserOptions,
+} from 'holocene-timestamp-parser';
 
 
 
 const handleNode = (node: Element) => {
     node.childNodes.forEach((childNode) => {
         if (childNode.nodeType === 3 && childNode.nodeValue) {
-            const parser = new HoloceneParser(childNode.nodeValue);
-            const textHE = parser.textHE();
-            if (textHE) {
-                childNode.nodeValue = textHE;
+            const parser = new HoloceneTimestampParser(childNode.nodeValue);
+            const options: HoloceneTimestampParserOptions = {
+                insertBetween: 'NOTHING',
+                insertLocation: 'AFTER',
+                styleHETimestamp: 'REGULAR',
+                linkHE: false,
+                removeHE: false,
+                replaceTimestamp: true,
+            }
+            const text = parser.text(options);
+            if (text.HE) {
+                console.log('text.HE', text.HE);
+                childNode.nodeValue = text.HE;
             }
             // console.log(childNode.nodeType);
             // console.log(childNode.nodeValue);
