26d922fa0af8e723648d5750094dcca99d6d1149
Jan Cavel <caveljan@gmail.com>
2019-08-04 14:03:03 +0300

    holocene-timestamp-parser > dev: constants

--- packages/holocene-timestamp-parser/src/constants/index.ts
+++ packages/holocene-timestamp-parser/src/constants/index.ts
@@ -1,7 +1,5 @@
-export const HE_TIMEBASE = 10000;
-
 export const SPACE_SEPARATOR = ' ';
 
-export const WIKIPEDIA_HE_LINK = 'https://en.wikipedia.org/wiki/Holocene_calendar';
-
-export const HE_ANCHOR = `<a href="${WIKIPEDIA_HE_LINK}">HE</a>`;
+export const HE_TIMEBASE = 10000;
+export const HE_LINK_WIKIPEDIA = 'https://en.wikipedia.org/wiki/Holocene_calendar';
+export const HE_ANCHOR = `<a href="${HE_LINK_WIKIPEDIA}">HE</a>`;
